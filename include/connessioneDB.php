948894a213d7c3732caba4c8b776253aab471b50
alfatauri10 <martucci.valentina@itisgalileiroma.it>
2026-03-24 19:08:00 +0100

    prova push

--- include/connessioneDB.php
+++ include/connessioneDB.php
@@ -11,6 +11,7 @@
     // 1. Inizializza mysqli
     $conn = mysqli_init();
 
+
     // 2. Se hai scaricato il certificato CA da Aiven (consigliato), indicalo qui.
     // Se vuoi solo forzare l'SSL senza verificare il certificato:
     mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
