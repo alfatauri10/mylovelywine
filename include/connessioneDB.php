25d8bc2e5f0f73170df6babb81ea41e71f6755b9
alfatauri10 <martucci.valentina@itisgalileiroma.it>
2026-03-24 23:36:02 +0100

    prova DB AIVEN da web server altervista

--- include/connessioneDB.php
+++ include/connessioneDB.php
@@ -1,16 +1,14 @@
 <?php
     //$conn = mysqli_connect("localhost","root","","my_vmartucci");
 
-
     $host = "mylovelywine-db-mylovelywine.a.aivencloud.com";
     $user = "avnadmin";
-    $pass = "AVNS_GmH8d95xhk-qpUwYaB8"; // Verifica che sia corretta
+    $pass = "AVNS_GmH8d95xhk-qpUwYaB8";
     $db = "defaultdb";
     $port = 11228;
 
     // 1. Inizializza mysqli
     $conn = mysqli_init();
-
 
     // 2. Se hai scaricato il certificato CA da Aiven (consigliato), indicalo qui.
     // Se vuoi solo forzare l'SSL senza verificare il certificato:
