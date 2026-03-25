a6e0379e6e4c52433783cea22e12b2c35f49db9b
alfatauri10 <martucci.valentina@itisgalileiroma.it>
2026-03-25 22:07:32 +0100

    prova DB AIVEN da web server altervista

--- include/connessioneDB.php
+++ include/connessioneDB.php
@@ -1,19 +1,31 @@
 <?php
-    //$conn = mysqli_connect("localhost","root","","my_vmartucci");
-
     $host = "mylovelywine-db-mylovelywine.a.aivencloud.com";
     $user = "avnadmin";
     $pass = "AVNS_GmH8d95xhk-qpUwYaB8";
-    $db = "defaultdb";
+    $db   = "defaultdb";
     $port = 11228;
 
+    // 1. Inizializziamo la connessione in modo speciale per l'SSL
+    $conn = mysqli_init();
 
-    $conn = mysqli_connect($host, $user ,$pass,$db ,$port);
+    // 2. Diciamo a PHP che la connessione DEVE essere cifrata (SSL)
+    // Su Altervista non serve il certificato fisico .pem, basta forzare l'uso dell'SSL
+    mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
+    // 3. Proviamo a connetterci
+    $success = mysqli_real_connect(
+        $conn,
+        $host,
+        $user,
+        $pass,
+        $db,
+        $port,
+        NULL,
+        MYSQLI_CLIENT_SSL
+    );
 
-    if(!$conn){
-        die("Connessione fallitaa: " . mysqli_connect_error());
+    if (!$success) {
+        die("Connessione fallita: " . mysqli_connect_error());
     }
-
 
 ?>
