3dd0502e25ac7e2eb49116a4cdbcbac9842c015c
alfatauri10 <martucci.valentina@itisgalileiroma.it>
2026-03-25 19:30:08 +0100

    prova DB AIVEN da web server altervista

--- include/connessioneDB.php
+++ include/connessioneDB.php
@@ -7,31 +7,13 @@
     $db = "defaultdb";
     $port = 11228;
 
-    $conn = mysqli_init();
 
-    // 2. Imposta il percorso del certificato (anche se lo ignoreremo, serve a mysqli per attivare l'SSL)
-    $ca_cert = __DIR__ . "/ca.pem";
+    $conn = mysqli_connect($host, $user ,$pass,$db ,$port);
 
-    // Configuriamo l'SSL. Se il file non venisse letto bene, il flag sotto ci salverà.
-   // mysqli_ssl_set($conn, NULL, NULL, $ca_cert, NULL, NULL);
 
-    // 3. Tenta la connessione con i FLAG COMBINATI
-    // Il simbolo "|" unisce i due comandi: "Usa SSL" + "Non verificare il certificato"
-    $success = mysqli_real_connect(
-        $conn,
-        $host,
-        $user,
-        $pass,
-        $db,
-        $port,
-        NULL,
-        MYSQLI_CLIENT_SSL | MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT
-    );
-
-    if (!$success) {
-        die("Errore critico di connessione: " . mysqli_connect_error());
+    if(!$conn){
+        die("Connessione fallitaa: " . mysqli_connect_error());
     }
 
-    // Se arrivi qui, è fatta!
-    // echo "Connessione stabilita con successo!";
-    ?>
+
+?>
